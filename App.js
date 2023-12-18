ae3d6fc2f00a5f5c668475777ee4defdecc2ad23
bechara0 <jjbechara@gmail.come>
2023-12-18 17:42:44 -0300

    terminado

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
-import { StyleSheet, Text, View, Platform, SafeAreaView } from "react-native";
-import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import { useState, useEffect } from "react";
 import Header from "./src/components/Header";
 import { Timer } from "./src/components/Timer";
+import { Audio } from "expo-av";
 
 const colors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"];
 
@@ -9,6 +17,41 @@ export default function App() {
   const [isWorking, setIsWorking] = useState(false);
   const [time, setTime] = useState(25 * 60);
   const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREACK");
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive) {
+      //run timer
+      interval = setInterval(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else {
+      //clear interval
+      clearInterval(interval);
+    }
+
+    if (time === 0) {
+      setIsActive(false);
+      setIsWorking((prev) => !prev);
+      setTime(isWorking ? 300 : 1500);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, time]);
+
+  const handleStartStop = () => {
+    playSound();
+    setIsActive(!isActive);
+  };
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/alerta.mp3")
+    );
+    await sound.playAsync();
+  }
 
   return (
     <SafeAreaView
@@ -28,6 +71,11 @@ export default function App() {
           setTime={setTime}
         />
         <Timer time={time} />
+        <TouchableOpacity onPress={handleStartStop} style={styles.button}>
+          <Text style={{ color: "white", fontWeight: "bold" }}>
+            {isActive ? "STOP" : "START"}
+          </Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -40,5 +88,12 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 32,
     fontWeight: "bold",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#333333",
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
   },
 });
